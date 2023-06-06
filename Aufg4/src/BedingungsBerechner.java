42c434499f2df0f6906efc9eb888654ce6604bd9
Gramos Luzha <gramos.luzha@haw-hamburg.de>
2023-06-06 10:26:09 +0200

    zoertified

--- Aufg4/src/BedingungsBerechner.java
+++ Aufg4/src/BedingungsBerechner.java
@@ -8,7 +8,7 @@ import java.util.Set;
 
 public class BedingungsBerechner {
 
-    Set<List<String>> ergebnisWerte = new HashSet<>();
+    Set<List<String>> ergebnisSet = new HashSet<>();
 
     public static void main(String[] args) throws FileNotFoundException {
         BedingungsBerechner bedingungsBerechner = new BedingungsBerechner();
@@ -16,111 +16,106 @@ public class BedingungsBerechner {
         String neueMD = "C:\\Users\\G\\Desktop\\CT\\Praktikumsaufgaben\\Material-Aufg4\\exercises\\test2.md";
 
         bedingungsBerechner.mcdc(exercize);
-        bedingungsBerechner.mdTabelleErstellen(neueMD, bedingungsBerechner.getErgebnisWerte());
+        bedingungsBerechner.mdTabelleErstellen(neueMD, bedingungsBerechner.getErgebnisSet());
 
     }
 
-        private TabellenLeser leser = new TabellenLeser();
+    private TabellenLeser leser = new TabellenLeser();
 
-        public boolean isNachbar(List<String> zeile1, List<String> zeile2) {
-            int count = 0;
+    public boolean istNachbar(List<String> zeile1, List<String> zeile2) {
+        int noMatch = 0;
 
-            for (int i = 0; i < zeile1.size() - 1; i++) {
-                if (!zeile1.get(i).equals(zeile2.get(i))) {
-                    count++;
-                }
+        for (int i = 0; i < zeile1.size() - 1; i++) {
+            if (!zeile1.get(i).equals(zeile2.get(i))) {
+                noMatch++;
             }
-
-            if (count > 1) {
-                return false;
-            }
-
-            return true;
         }
 
-        public List<List<String>> findAllNeighbours(List<String> zeile, List<List<String>> liste) {
-
-            List<List<String>> neighbours = new ArrayList<>();
-
-            for (int i = 0; i < liste.size(); i++) {
-                if (isNachbar(zeile, liste.get(i))) {
-                    neighbours.add(liste.get(i));
-                }
-            }
-            return neighbours;
+        if (noMatch > 1) {
+            return false;
         }
 
+        return true;
+    }
+
+    public List<List<String>> getNachbarn(List<String> zeile, List<List<String>> liste) {
+
+        List<List<String>> nachbarn = new ArrayList<>();
+
+        for (int i = 0; i < liste.size(); i++) {
+            if (istNachbar(zeile, liste.get(i))) {
+                nachbarn.add(liste.get(i));
+            }
+        }
+        return nachbarn;
+    }
 
 
-        public Set<List<String>> mmbue(String file) throws FileNotFoundException {
+    public Set<List<String>> mmbue(String file) throws FileNotFoundException {
 
-            List<List<String>> werteListe = leser.leseTabelle(file);
-
-
-            int werteListeSize = werteListe.size();
-            int werteListeZeileSize = werteListe.get(0).size();
+        List<List<String>> werteListe = leser.leseTabelle(file);
 
 
-            for (int zeile = 0; zeile < werteListeSize; zeile++){
-                List<List<String>> neighbours = findAllNeighbours(werteListe.get(zeile), werteListe);
+        int werteListeSize = werteListe.size();
+        int werteListeZeileSize = werteListe.get(0).size();
 
-                for (int n = 0; n < neighbours.size(); n++) {
-                    if (!werteListe.get(zeile).get(werteListeZeileSize - 1).equals(neighbours.get(n).get(werteListeZeileSize - 1))) {
-                        ergebnisWerte.add(neighbours.get(n));
-                    }
+
+        for (int zeile = 0; zeile < werteListeSize; zeile++) {
+            List<List<String>> nachbarn = getNachbarn(werteListe.get(zeile), werteListe);
+
+            for (List<String> nachbar : nachbarn) {
+                if (!werteListe.get(zeile).get(werteListeZeileSize - 1).equals(nachbar.get(werteListeZeileSize - 1))) {
+                    ergebnisSet.add(nachbar);
                 }
             }
-            ergebnisWerte.forEach(System.out::println);
-            return ergebnisWerte;
         }
+        ergebnisSet.forEach(System.out::println);
+        return ergebnisSet;
+    }
 
-        public void mcdc(String file) throws FileNotFoundException {
+    public void mcdc(String file) throws FileNotFoundException {
 
-            List<List<String>> werteListe = leser.leseTabelle(file);
-            Set<List<String>> ergebnisWerte = new HashSet<>();
+        List<List<String>> werteTabelle = leser.leseTabelle(file);
+        Set<List<String>> ergebnisSet = new HashSet<>();
 
-            int werteListeSize = werteListe.size();
-            int werteListeZeileSize = werteListe.get(0).size();
-
-
-
-            for (int zeile = 0; zeile < werteListeSize; zeile++){
-                List<List<String>> neighbours = findAllNeighbours(werteListe.get(zeile), werteListe);
-
-                for (int n = 0; n < neighbours.size(); n++) {
-                    if (!werteListe.get(zeile).get(werteListeZeileSize - 1).equals(neighbours.get(n).get(werteListeZeileSize - 1))) {
-                        ergebnisWerte.add(neighbours.get(n));
-
-                    }
+        int werteTabelle_size = werteTabelle.size();
+        int werteTabeleZeile_size = werteTabelle.get(0).size();
 
 
+        for (int zeile = 0; zeile < werteTabelle_size; zeile++) {
+            List<List<String>> nachbarn = getNachbarn(werteTabelle.get(zeile), werteTabelle);
+
+
+            for (List<String> strings : nachbarn) {
+                if (!werteTabelle.get(zeile).get(werteTabeleZeile_size - 1).equals(strings.get(werteTabeleZeile_size - 1))) {
+                    ergebnisSet.add(strings);
                 }
             }
-            System.out.println(ergebnisWerte);
         }
+        System.out.println(ergebnisSet);
+    }
 
     public void mdTabelleErstellen(String filePath, Set<List<String>> tableData) {
         try (FileWriter writer = new FileWriter(filePath)) {
-            // Write the Markdown table header
             writer.write("| A | B | C | Ergebnis |\n");
             writer.write("| -------- | -------- | -------- | -------- |\n");
 
-            // Write the table data
+
             for (List<String> rowData : tableData) {
                 writer.write("| " + rowData.get(0) + " | " + rowData.get(1) + " | " + rowData.get(2) + " | " + rowData.get(3) + " |\n");
             }
 
-            System.out.println("Markdown table generated successfully!");
+            System.out.println("Markdowntabelle generiert");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    public Set<List<String>> getErgebnisWerte() {
-            return ergebnisWerte;
+    public Set<List<String>> getErgebnisSet() {
+        return ergebnisSet;
     }
 
-    }
+}
 
 
